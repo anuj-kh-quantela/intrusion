0d6b515d71480974b166501dbe5d6a088edb048c
Anuj Khandelwal <anuj.khandelwal@quantela.com>
2018-04-24 17:09:48 +0530

    Plot=False

--- run_intrusion.py
+++ run_intrusion.py
@@ -9,7 +9,7 @@ def wrapper(video_channel, city_name, location_name, roi):
 	intr = intrusion(video_channel, city_name, location_name, ROI=roi)
 	# intr = intrusion('test_video_1.mp4', 'vijaywada', 'datacenter', ROI=roi)
 	# intr = intrusion('test_video_1.mp4', 'bangalore', "indranagar/society-2")
-	intr.detect_intrusion(plot=True)
+	intr.detect_intrusion(plot=False)
 
 video_channel_arr = ['rtsp://192.168.20.9/6d801f1f-a9aa-449a-85d3-88608e5ee67b/6d801f1f-a9aa-449a-85d3-88608e5ee67b_vs1?token=6d801f1f-a9aa-449a-85d3-88608e5ee67b^LVERAMOTD^50^26^26^1657790795^d660cf85eebea453b0c933b63025aedeb9c22fea&username=admin']
 location_name_arr = ['datacenter']
