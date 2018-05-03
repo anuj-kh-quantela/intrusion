80ba13b38002a893355df3c9bd850d574b48a046
Anuj Khandelwal <anuj.khandelwal@quantela.com>
2018-05-03 15:06:12 +0530

    changes

--- intrusion.py
+++ intrusion.py
@@ -310,5 +310,5 @@ class intrusion(object):
 
 			detected_roi = {}
 
-intr = intrusion('test_video_1.mp4', 'kanpur')
-intr.detect_intrusion(plot=True)
+# intr = intrusion('test_video_1.mp4', 'kanpur')
+# intr.detect_intrusion(plot=True)
